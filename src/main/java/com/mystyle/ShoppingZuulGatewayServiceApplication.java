5770dd87c89ba788973353b49301efd0f1154489
chandan <chandan@192.168.0.102>
2019-09-10 17:56:23 +0530

    commit

--- src/main/java/com/mystyle/ShoppingZuulGatewayServiceApplication.java
+++ src/main/java/com/mystyle/ShoppingZuulGatewayServiceApplication.java
@@ -2,8 +2,16 @@ package com.mystyle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Import;
+
+import com.mystyle.config.WebConfig;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@Import(WebConfig.class)
 public class ShoppingZuulGatewayServiceApplication {
 
 	public static void main(String[] args) {
