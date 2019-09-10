2a2ecad5746bf494543905d1a414f68c7f33ea10
chandan <chandan@192.168.0.102>
2019-09-10 17:18:42 +0530

    initial commit

---
+++ src/main/java/com/mystyle/ShoppingZuulGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mystyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingZuulGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingZuulGatewayServiceApplication.class, args);
+	}
+
+}
