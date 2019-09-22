979fdcec7a800b33b18c31efc0d431098cf3cd7e
chandan <chandan@192.168.0.103>
2019-09-22 17:50:56 +0530

    changed jar file name

--- src/main/java/com/mystyle/ShoppingZuulGatewayServiceApplication.java
+++ src/main/java/com/mystyle/ShoppingZuulGatewayServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 import com.mystyle.config.WebConfig;
 
@@ -12,6 +13,7 @@ import com.mystyle.config.WebConfig;
 @EnableZuulProxy
 @EnableEurekaClient
 @Import(WebConfig.class)
+@PropertySource("classpath:application.yml")
 public class ShoppingZuulGatewayServiceApplication {
 
 	public static void main(String[] args) {
